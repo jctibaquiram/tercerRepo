04472b9cfd589d8191b35bf5048b1b34c282b47e
jctibaquiram09 <jctibaquiram09@gmail.com>
2026-05-14 18:07:58 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Juan Tibaquira",                         # Tu nombre
+    author_email="juan.tibaquira24@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/jctibaquiram/tercerRepo",     # URL del proyecto
+)
